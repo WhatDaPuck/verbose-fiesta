11959571c34e66585b9dca6571d599d370b244b4
WhatDaPuck <cbock1337@gmail.com>
2017-08-01 21:22:21 -0400

    test

--- numbercounttest/numbercounttest.cpp
+++ numbercounttest/numbercounttest.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include <Windows.h>
 
+int func()
+{
+	int base, exponent, stop;
+	stop = 1000000;
+	long long result = 0;
+	base = 1;
+	exponent = stop;
+	while (result < stop)
+	{
+		result += base;
+		//printf("%d\n", result);  //uncomment if you want
+	}
+	return 0;
+}
 
 int main()
 {
+	DWORD tSTART = __rdtsc();
+	func();
+	DWORD tSTOP = __rdtsc();
+	DWORD elapsed = tSTOP - tSTART;
+	std::cout << "Your beginning tick was: " << (tSTART) << '\n' << "Your stopping tick was: " << (tSTOP) << '\n' << "The total difference was: " << (elapsed);
+	getchar();
     return 0;
 }
-
